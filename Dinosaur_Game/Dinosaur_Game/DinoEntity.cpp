4d94bebf31a178485dc0747304506ae742473d2b
Mooooon-s <mmk990212@naver.com>
2022-07-31 20:36:31 +0900

    commit

--- Dinosaur_Game/Dinosaur_Game/DinoEntity.cpp
+++ Dinosaur_Game/Dinosaur_Game/DinoEntity.cpp
@@ -9,17 +9,28 @@ void DinoEntity::drawEntity()
 {	//대충 모양만 나중에 수정
 	Game_manager* gm = new Game_manager;
 	gm->gotoXY(0, Y_BASE - _Y);	// 공룡 그리기, 커서 위치 변경
-	std::cout << "    ■■\n";
-	std::cout << "    ■\n";
-	std::cout << "■■■■\n";
+	std::cout << "        ■■■■ \n";
+	std::cout << "       ■ ■■■\n";
+	std::cout << "      ■■■■■\n";
+	std::cout << "■     ■■      \n";
+	std::cout << "■     ■■■■  \n";
+	std::cout << "■■   ■■■     \n";
+	std::cout << " ■■■■■■■ \n";
+	std::cout << " ■■■■■    \n";
+	std::cout << "  ■■■■■    \n";
+	std::cout << "    ■■■■    \n";
+	std::cout << "     ■■■     \n";
+
 	if (_leg) {
-		std::cout << "      ■\n";
+		std::cout << "  ■   ■■\n";
+		std::cout << "  ■■   \n";
 		_leg = false;
 	}
 	else {
-		std::cout << "  ■  \n";
+		std::cout << "  ■■ ■ \n";
+		std::cout << "       ■■ \n";
 
 		_leg = true;
 	}
 	delete gm;
-}
+} 
--- Dinosaur_Game/Dinosaur_Game/setting.h
+++ Dinosaur_Game/Dinosaur_Game/setting.h
@@ -3,7 +3,7 @@ const int KEY_ESC = 27;			// ESC 키
 const int KEY_SPACE = 32;		// SpaceBar 키
 
 const int MAX_JUMP = 6;			// 최대 점프 높이
-const int Y_BASE = 20 ;			// 공룡의 초기 Y축 위치
+const int Y_BASE = 11 ;			// 공룡의 초기 Y축 위치
 const int Y_COLLISION = 4;		// Y축의 충돌 기준 위치
 const int TREE_COLLISION = 7;	// 나무가 공룡과 충돌 가능한 X축 위치
 const int TREE_START = 50;		// 나무가 생성되는 위치
